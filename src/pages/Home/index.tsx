c8b469e73187832e1ee46e82e9f473340e9b955c
최민우 <woasidh1@naver.com>
2021-09-20 00:55:24 +0900

    login page mark up scss webpack 설정 전 commit

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, {ReactElement} from 'react';
 import './index.css'
 
-const Index = () => {
+const Index = (): ReactElement => {
     return (
         <div className = "tmp_text">this is Home!</div>
     )
 }
-
 export default Index;
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import logo from '../../resource/images/logo.png';
 
 const Index = () => {
     return (
-        <div>
-            this is Login Page!
+        <div className = "login_page">
+            <div className = "container">
+                <img src = {logo} alt = "logo"/>
+                <div>로그인</div>
+                <div className = "input_text" id = "email">이메일</div>
+                <input type = "text"/>
+                <div className = "input_text" id = "pwd">비밀번호</div>
+                <input type = "text"/>
+                <button>로그인</button>
+                <input type = "checkbox"/>
+                <span>로그인 상태 유지할래요</span>
+                <button>비민번호를 잊어버렸어요</button>
+                <span>아직 회원이 아니신가요?</span>
+                <button>회원 가입하기</button>
+            </div>
         </div>
     );
 }
