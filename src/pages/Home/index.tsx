19361ead28b265327be767dc295ca7cd17cc1536
최민우 <woasidh1@naver.com>
2021-09-21 00:56:20 +0900

    login page html, css 완료

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,4 @@
 import React, {ReactElement} from 'react';
-import './index.css'
 
 const Index = (): ReactElement => {
     return (
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,22 +1,37 @@
 import React from 'react';
 import logo from '../../resource/images/logo.png';
+import './index.scss';
 
 const Index = () => {
     return (
-        <div className = "login_page">
+        <div className = "login">
             <div className = "container">
-                <img src = {logo} alt = "logo"/>
-                <div>로그인</div>
-                <div className = "input_text" id = "email">이메일</div>
-                <input type = "text"/>
-                <div className = "input_text" id = "pwd">비밀번호</div>
-                <input type = "text"/>
-                <button>로그인</button>
-                <input type = "checkbox"/>
-                <span>로그인 상태 유지할래요</span>
-                <button>비민번호를 잊어버렸어요</button>
-                <span>아직 회원이 아니신가요?</span>
-                <button>회원 가입하기</button>
+                <div className = "header">
+                    <img src = {logo} alt = "logo"/>
+                    <div>로그인</div>
+                </div>
+                <div className = "content">
+                    <div className = "login_input" id = "email">
+                        <span>이메일</span>
+                        <input type = "text"/>
+                    </div>
+                    <div className = "login_input" id = "pwd">
+                        <span>비밀번호</span>
+                        <input type = "text"/>
+                    </div>
+                    <button>로그인</button>
+                    <div className = "login_keep">
+                        <input type = "checkbox" id = "keep_login"/>
+                        <label htmlFor = "keep_login" id = "custom_checkbox">로그인 상태 유지할래요</label>
+                    </div>
+                </div>
+                <div className = "footer">
+                    <button className = "lost_pwd">비민번호를 잊어버렸어요</button>
+                    <div className = "signup">
+                        <span>아직 회원이 아니신가요?</span>
+                        <button>회원 가입하기</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
