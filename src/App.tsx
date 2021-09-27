5e881ed4543a0670f49b226f1610b4a58d31dee4
최민우 <woasidh1@naver.com>
2021-09-27 22:05:47 +0900

    signup router에 추가 및 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,15 @@ import {
 } from 'react-router-dom';
 import Home from './pages/Home/index';
 import Login from './pages/Login/index';
+import Signup from "./pages/Signup/Index";
 
 function App() {
   return (
       <Router>
           <Switch>
-              <Route exact path = '/'>
-                  <Home/>
-              </Route>
-              <Route exact path = '/login'>
-                  <Login/>
-              </Route>
+              <Route exact path = '/'><Home/></Route>
+              <Route exact path = '/login'><Login/></Route>
+              <Route exact path = '/signup'><Signup/></Route>
           </Switch>
       </Router>
   );
