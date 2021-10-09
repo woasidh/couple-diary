c604b5ffbd70f6d76d0bed0ceb7faa1f6aeb3216
최민우 <woasidh1@naver.com>
2021-10-09 13:44:31 +0900

    connect page window width, height 랜덤으로 좌표 구하기 로직 구현

--- src/pages/Connect/Index.tsx
+++ src/pages/Connect/Index.tsx
@@ -6,6 +6,7 @@ import {Point} from "../../util/Point";
 const Connect = () => {
 
     const imgSize = useRef<number>(50);
+    const container = useRef<HTMLDivElement>(null);
 
     function makeHeart(): void {
         console.log('하트 그려짐');
@@ -19,25 +20,26 @@ const Connect = () => {
         const validWidth = window.innerWidth - imgSize.current;
         const validHeight = window.innerHeight - imgSize.current;
 
+        console.log(window.innerWidth, window.innerHeight, Math.random() * validWidth, Math.random() * validHeight);
+
         return ({
             x: Math.random() * validWidth,
             y: Math.random() * validHeight,
         });
     }
 
-    setInterval(() => {
-        const container1 = document.querySelector('.connect_root');
-        const img = document.createElement('img');
-        img.src = Heart;
-        img.alt = "a";
-        img.width = 50;
-        img.style.transform = `translate(${getRandomCoordinate().x}px, ${getRandomCoordinate().y}px)`;
-        console.log(container1);
-        if (container1) container1.appendChild(img);
-    }, 1000);
+    // setInterval(() => {
+    //     const img = document.createElement('img');
+    //     img.src = Heart;
+    //     img.alt = "a";
+    //     img.width = 50;
+    //     img.style.transform = `translate(${getRandomCoordinate().x}px, ${getRandomCoordinate().y}px)`;
+    //     container.current?.appendChild(img);
+    // }, 1000);
 
     return (
         <div className = 'connect_root'>
+            <div ref = {container} className = 'heart_image_wrap'/>
         </div>
     );
 }
