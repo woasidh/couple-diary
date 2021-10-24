7247c1da72265e12d75b879bcbe897f87314cefd
최민우 <mwchoi@miridih.com>
2021-10-24 22:00:18 +0900

    HOC 조건 적용

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,7 @@ import Workspace from './pages/Workspace/Index';
 import Fallback from './pages/Fallback/Index';
 import Topbar from './components/Topbar/Topbar';
 import Connect from './pages/Connect/Index';
-import Unauthorized from './pages/Unauthorized/Unauthorized';
-import { HOC } from './util/HOC';
+import { AuthOption, HOC } from './util/HOC';
 
 function App(): ReactElement {
   return (
@@ -21,11 +20,11 @@ function App(): ReactElement {
       <Topbar />
       <Router>
         <Switch>
-          <Route exact path="/" component = {Login}></Route>
-          <Route exact path="/workspace" component = {HOC.fallbackLoginPage(Workspace)}></Route>
-          <Route exact path="/calendar" component = {HOC.fallbackLoginPage(Home)}></Route>
+          <Route exact path="/" component = {HOC.checkAuth(Login, AuthOption.NO_AUTH_ONLY)}></Route>
+          <Route exact path="/workspace" component = {HOC.checkAuth(Workspace, AuthOption.AUTH_ONLY)}></Route>
+          <Route exact path="/calendar" component = {HOC.checkAuth(Home, AuthOption.AUTH_ONLY)}></Route>
           <Route exact path="/signup"><Signup /></Route>
-          <Route exact path="/connect" component = {HOC.fallbackLoginPage(Connect)}></Route>
+          <Route exact path="/connect" component = {HOC.checkAuth(Connect, AuthOption.AUTH_ONLY)}></Route>
           <Route path="*"><Fallback /></Route>
         </Switch>
       </Router>
--- src/util/HOC.tsx
+++ src/util/HOC.tsx
@@ -1,34 +1,41 @@
 import axios from 'axios';
-import React, {ReactElement, useEffect } from 'react';
-import Unauthorized from '../pages/Unauthorized/Unauthorized';
+import React, { ReactElement, useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
+
+export enum AuthOption {
+  AUTH_ONLY,
+  NO_AUTH_ONLY
+}
 
 export namespace HOC {
   // TODO 로그인되었는지 확인하는 HOC 구현하기
-  export function fallbackLoginPage(Component: () => ReactElement): () => ReactElement {
-    
-    let isLoggedIn: boolean;
-    axios.get('/api/users/login/check').then((res) => {
-      isLoggedIn = res.data.isLoggedIn;
-    });
-    
+  export function checkAuth(
+    Component: () => ReactElement,
+    option: AuthOption): () => ReactElement {
+
     const NewComponent = (): ReactElement => {
+
+      const [isLoading, setIsLoading] = useState<boolean>(true);
+
+      const history = useHistory();
+
+      useEffect(() => {
+        axios.get('/api/users/login/check').then((res) => {
+          // 로그인유저만 출입가능 - 로그인 안되어있을 떄 -> 로그인으로
+          if (!res.data.isLoggedIn && option === AuthOption.AUTH_ONLY) history.push('/');
+          // 로그인안된 유저만 출입가능 - 로그인 되어있을 때 -> workspace로 (현재는 loginPage만 적용)
+          else if (res.data.isLoggedIn && option === AuthOption.NO_AUTH_ONLY) history.push('/workspace');
+          // 나머지는 그대로
+          setIsLoading(false);
+        });
+      }, []);
+
       return (
         <>
-        {isLoggedIn ? <Component/> : <Unauthorized/>}
+        {!isLoading && <Component />}
         </>
       );
     };
-
-    return NewComponent;
-  }
-
-  export function returnSameComponent(Component: () => ReactElement): () => ReactElement {
-
-    const NewComponent = (): ReactElement => {
-      return (
-        <Component/>
-      );
-    }
 
     return NewComponent;
   }
