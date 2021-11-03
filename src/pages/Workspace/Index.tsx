7c5f298038ff58406f6f9c5e8cd20d36af0e5fbc
최민우 <woasidh1@naver.com>
2021-11-04 00:37:55 +0900

    로그아웃 redux 상태 유효성 체크

--- src/pages/Workspace/Index.tsx
+++ src/pages/Workspace/Index.tsx
@@ -1,23 +1,37 @@
-import React, { ReactElement } from 'react';
+import React, {ReactElement} from 'react';
 import './Index.scss';
-import { useHistory } from 'react-router-dom';
+import {useHistory} from 'react-router-dom';
 import AgoSad from '../../resource/images/ago_sad.png';
+import {useSelector} from 'react-redux';
+import {RootState} from '../../redux_module';
 
 const Workspace = (): ReactElement => {
   const history = useHistory();
+  const isCouple = useSelector((state: RootState) => state.user?.isCouple);
 
   function onClickConnectCouple(): void {
     history.push('connect');
   }
 
   return (
-    <div className="workspace_root">
-      <div className="single_info_container">
-        <img src={AgoSad} alt="ago_sad" />
-        <div className="solo_desc">아직 커플이 아니에요...</div>
-        <button onClick={onClickConnectCouple} className="link_connect">연결하러 가기</button>
+    <>
+      <div className="workspace_root">
+        {
+          !isCouple &&
+          <div className="single_info_container">
+            <img src={AgoSad} alt="ago_sad"/>
+            <div className="solo_desc">아직 커플이 아니에요...</div>
+            <button onClick={onClickConnectCouple} className="link_connect">연결하러 가기</button>
+          </div>
+        }
+        {
+          isCouple &&
+          <div>
+            워크스페이스에 오신걸 환영합니다!
+          </div>
+        }
       </div>
-    </div>
+    </>
   );
 };
 
